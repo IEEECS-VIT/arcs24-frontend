49a3dae089759d549fb98d80db56bcf6ffb40271
Ansh Mehta <anshkapilmehta@gmail.com>
2024-06-23 17:00:26 +0400

    tailwind config configured with fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
-    theme: {
-        extend: {},
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ["Nunito Sans", "sans-serif"],
+        "source-serif": ['"Source Serif 4"', "serif"],
+        "pt-sans": ["PT Sans", "sans-serif"],
+        neuton: ["Neuton", "serif"],
+        rugen: ["Rugen", "sans-serif"],
+      },
+      colors: {
+        "bright-pink": "#B31AD6",
+        "dull-pink": "#8E4497",
+        blurple: "#9F9DFF",
+      },
     },
     plugins: [],
-}
-
+  },
+};
