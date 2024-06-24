447d0787a307dafa671fbec07ce20044eb81745d
Aryan Jain <aryanrameshjain@gmail.com>
2024-06-24 22:42:01 +0530

    complete unresponsive page

--- src/Yooho.jsx
+++ src/Yooho.jsx
@@ -3,8 +3,18 @@ import "./index.css";
 
 function Yooho() {
     return (
-        <div className="flex items-center justify-center min-h-screen animated-gradient gradient">
-            <img className="max-w-full h-auto" src="/yoho.png" />
+        <div className="flex items-center justify-center min-h-screen animated-gradient">
+            <div className="relative">
+                <img className="max-w-full h-auto" src="/yoho.png" alt="Yoho" />
+                <div className="text-overlay">
+                    <span>TWO</span>
+                    <span>CREATIVE</span>
+                    <span>EVENTS FOR THE</span>
+                    <span>NEXT</span>
+                    <span>WAVE OF</span>
+                    <span>LOCAL TALENT</span>
+                </div>
+            </div>
         </div>
     );
 }
