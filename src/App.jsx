76457f88f0b2f40098a6f0e77aa1c097f9897bc4
Ansh Mehta <anshkapilmehta@gmail.com>
2024-06-25 12:58:33 +0400

    fixing conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import React from "react";
-import Component1 from "./Component1";
 
 function App() {
-  return (
-    <main>
-      <Component1></Component1>
-      <Component1></Component1>
-    </main>
-  );
+  return <main></main>;
 }
 
 export default App;
