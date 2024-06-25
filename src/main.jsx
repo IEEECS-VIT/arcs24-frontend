bbc25078477d4e41bdfa9d109de8494799281787
Ansh Mehta <anshkapilmehta@gmail.com>
2024-06-25 13:07:41 +0400

    background colour

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
-    <App />
   </React.StrictMode>,
 );
