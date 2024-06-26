916eae2d46107d64e17e7949829508e2f54446c7
Tithi Shah <shahtithi21@gmail.com>
2024-06-26 18:22:44 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 function App() {
-  return <main></main>;
+  return <main>
+    <h1>HII</h1>
+    <img src="echoelipse.svg" alt="ech" height="250px" width="250px"/>
+  </main>;
 }
 
 export default App;
