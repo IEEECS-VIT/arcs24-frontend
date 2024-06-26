6ab8792eb21c8bcf852d1ed765ce2b828d9dc133
Ansh Mehta <anshkapilmehta@gmail.com>
2024-06-26 20:39:51 +0400

    Refactored and combined components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import React from "react";
-import Yooho from "./Yooho";
+import Yooho from "./Components/Yooho";
+import LandingPage from "./Components/LandingPage";
+
 function App() {
-  return <Yooho />;
+  return (
+    <main>
+      <LandingPage />
+      <Yooho />
+    </main>
+  );
 }
 
 export default App;
