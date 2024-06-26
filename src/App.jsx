62864959cbd4e9c1a611187889ad94510e0aa7e8
Tithi Shah <shahtithi21@gmail.com>
2024-06-26 19:15:45 +0530

    ell

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,24 @@ import React from "react";
 
 function App() {
   return <main>
-    <h1>HII</h1>
-    <img src="echoelipse.svg" alt="echo" height="250px" width="250px"/>
-    <div className="flex justify-center items-center h-screen">
-      <p className="text-center font- text-2xl font-bold text-white">
-        OUR PREVIOUS SPNSORS
+    <div className="relative w-full h-96">
+      {/* Base image */}
+      <img
+        src="echoelipse.svg"
+        alt="Base image"
+        className="absolute w-[250px] [h-250px] object-cover"
+      />
+      
+      {/* Overlapping image */}
+      <img
+        src="echo.svg"
+        alt="Overlapping image"
+        className="absolute top-[60px] left-[60px] w-[130px] h-[130px] object-cover rounded-full overflow-hidden"
+      />
+    </div>
+    <div className="flex justify-center items-start h-screen">
+      <p className="text-center font-[1000] text-5xl tracking-[0.25em] font-sans text-[#F7F6F3]">
+        OUR PREVIOUS <br></br>SPONSORS
       </p>
     </div>
   </main>;
