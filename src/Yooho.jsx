230ad244d60914ec10d22171722c824a74443987
Ansh Mehta <anshkapilmehta@gmail.com>
2024-06-26 20:31:42 +0400

    rounded borders and padding

--- src/Yooho.jsx
+++ src/Yooho.jsx
@@ -1,23 +1,35 @@
 import React from "react";
-import "./index.css"; 
+import "./index.css";
 
 function Yooho() {
-    return (
-        <div className="flex items-center justify-center min-h-screen relative">
-            <div className="animated-gradient w-full h-screen absolute top-0 left-0"></div>
-            <div className="relative">
-                <img className="w-[90vw] h-[78vh]" src="/yoho.png" alt="Yoho" />
-                <div className="text-overlay absolute text-white font-rugen top-0 left-0 flex flex-wrap flex-col box-border">
-                    <span className="sm:text-[8vh] md:text-[8vh] lg:text-[8.67vh] self-start relative mr-0">TWO</span>
-                    <span className="sm:text-[8vh] md:text-[8vh] lg:text-[8.67vh] self-start relative mr-0">CREATIVE</span>
-                    <span className="sm:text-[8vh] md:text-[8vh] lg:text-[8.67vh] self-start relative mr-0">EVENTS FOR THE</span>
-                    <span className="sm:text-[8vh] md:text-[8vh] lg:text-[8.67vh] self-start relative mr-0">NEXT</span>
-                    <span className="sm:text-[8vh] md:text-[8vh] lg:text-[8.67vh] self-start relative mr-0">WAVE OF</span>
-                    <span className="sm:text-[8vh] md:text-[8vh] lg:text-[8.67vh] self-start relative mr-0">LOCAL TALENT</span>
-                </div>
-            </div>
+  return (
+    <div className="relative flex min-h-screen items-center justify-center">
+      <div className="animated-gradient absolute left-0 top-0 h-screen w-full"></div>
+      <div className="relative">
+        <img className="h-[75vh] w-[90vw]" src="/yoho.png" alt="Yoho" />
+        <div className="text-overlay absolute left-0 top-0 box-border flex flex-col flex-wrap font-rugen text-white">
+          <span className="relative mr-0 self-start rounded-xl pr-2 sm:text-[8vh] md:text-[8vh] lg:text-[8.67vh]">
+            TWO
+          </span>
+          <span className="relative mr-0 self-start rounded-xl pr-2 sm:text-[8vh] md:text-[8vh] lg:text-[8.67vh]">
+            CREATIVE
+          </span>
+          <span className="relative mr-0 self-start rounded-xl pr-2 sm:text-[8vh] md:text-[8vh] lg:text-[8.67vh]">
+            EVENTS FOR THE
+          </span>
+          <span className="relative mr-0 self-start rounded-xl pr-2 sm:text-[8vh] md:text-[8vh] lg:text-[8.67vh]">
+            NEXT
+          </span>
+          <span className="relative mr-0 self-start rounded-xl pr-2 sm:text-[8vh] md:text-[8vh] lg:text-[8.67vh]">
+            WAVE OF
+          </span>
+          <span className="relative mr-0 self-start rounded-xl pr-2 sm:text-[8vh] md:text-[8vh] lg:text-[8.67vh]">
+            LOCAL TALENT
+          </span>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Yooho;
