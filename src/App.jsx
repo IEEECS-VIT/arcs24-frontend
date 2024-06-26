fdda2cd8d60b49c26aa33cfebbbf2e55cc43f43c
Tithi Shah <shahtithi21@gmail.com>
2024-06-26 18:43:55 +0530

    new neww

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,12 @@ import React from "react";
 function App() {
   return <main>
     <h1>HII</h1>
-    <img src="echoelipse.svg" alt="ech" height="250px" width="250px"/>
-    
+    <img src="echoelipse.svg" alt="echo" height="250px" width="250px"/>
+    <div className="flex justify-center items-center h-screen">
+      <p className="text-center font- text-2xl font-bold text-white">
+        OUR PREVIOUS SPNSORS
+      </p>
+    </div>
   </main>;
 }
 
