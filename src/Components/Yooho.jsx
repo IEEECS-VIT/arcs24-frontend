e57f3199f83748faf42518eb25d691cf5438102d
Aryan Jain <aryanrameshjain@gmail.com>
2024-06-27 15:52:44 +0530

    Modify font size

--- src/Components/Yooho.jsx
+++ src/Components/Yooho.jsx
@@ -7,22 +7,22 @@ function Yooho() {
       <div className="relative">
         <img className="h-[75vh] w-[90vw]" src="/yoho.png" alt="Yoho" />
         <div className="text-overlay absolute left-0 top-0 box-border flex flex-col flex-wrap font-rugen text-white">
-          <span className="relative mr-0 self-start rounded-xl pr-2 sm:text-[8vh] md:text-[8vh] lg:text-[8.67vh]">
+          <span className="relative mr-0 self-start rounded-xl pr-2 sm:text-[8vh] md:text-[8vh] lg:text-[8.33vh]">
             TWO
           </span>
-          <span className="relative mr-0 self-start rounded-xl pr-2 sm:text-[8vh] md:text-[8vh] lg:text-[8.67vh]">
+          <span className="relative mr-0 self-start rounded-xl pr-2 sm:text-[8vh] md:text-[8vh] lg:text-[8.33vh]">
             CREATIVE
           </span>
-          <span className="relative mr-0 self-start rounded-xl pr-2 sm:text-[8vh] md:text-[8vh] lg:text-[8.67vh]">
+          <span className="relative mr-0 self-start rounded-xl pr-2 sm:text-[8vh] md:text-[8vh] lg:text-[8.33vh]">
             EVENTS FOR THE
           </span>
-          <span className="relative mr-0 self-start rounded-xl pr-2 sm:text-[8vh] md:text-[8vh] lg:text-[8.67vh]">
+          <span className="relative mr-0 self-start rounded-xl pr-2 sm:text-[8vh] md:text-[8vh] lg:text-[8.33vh]">
             NEXT
           </span>
-          <span className="relative mr-0 self-start rounded-xl pr-2 sm:text-[8vh] md:text-[8vh] lg:text-[8.67vh]">
+          <span className="relative mr-0 self-start rounded-xl pr-2 sm:text-[8vh] md:text-[8vh] lg:text-[8.33vh]">
             WAVE OF
           </span>
-          <span className="relative mr-0 self-start rounded-xl pr-2 sm:text-[8vh] md:text-[8vh] lg:text-[8.67vh]">
+          <span className="relative mr-0 self-start rounded-xl pr-2 sm:text-[8vh] md:text-[8vh] lg:text-[8.33vh]">
             LOCAL TALENT
           </span>
         </div>
