17cf4e02c22eef1e7146c668f62eb2aa650c2c59
ShubhamPrasad3012 <shubhamprasadclass10@gmail.com>
2024-06-27 14:35:58 +0530

    combined , fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import React from "react";
-import Part1 from "./components/Part1";
-import Part2 from "./components/Part2";
+import Combined from "./components/Combined";
+
 function App() {
     return (
-        <>
-        <img className="img9 absolute" src="/Ellipse1.svg" alt=""/>
-        <img className="img8 absolute ml-[79.42708333333333vw] h-[96.20733249051833vh]" src="/Ellipse2.svg" alt=""/>
-        <div className="d4 flex w-screen relative">
-            <Part1/>
-            <Part2/>
-        </div>
-        </>
+        <Combined/>
     )
 }
-
 export default App;
