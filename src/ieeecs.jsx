d8a47e953930e991c5a96a280532239605f48147
Tithi <145503991+tithishah21@users.noreply.github.com>
2024-06-28 12:57:56 +0530

    Update ieeecs.jsx

--- src/ieeecs.jsx
+++ src/ieeecs.jsx
@@ -6,40 +6,40 @@ const MyComponent = () => {
   return (
     <div className="main">
         <div className="first">
-            <img src="echoell.svg" class="echoell" alt="echoell"/>
-            <img src="echo.svg" class="echo" alt="echo"/>
+            <img src="echoell.svg" className="echoell" alt="echoell"/>
+            <img src="echo.svg" className="echo" alt="echo"/>
         </div>
         <div className="text">
-            <img src="textell.svg" class="textell" alt="textell"/>
-            <img src="our Previous sponsors.png" class="newsp" alt="newsp"/>
+            <img src="textell.svg" className="textell" alt="textell"/>
+            <img src="our Previous sponsors.png" className="newsp" alt="newsp"/>
         </div>
         <div className="sec">
-            <img src="axureell.svg" class="axureell" alt="axureell"/>
-            <img src="axure.svg" class="axure" alt="axure"/>
+            <img src="axureell.svg" className="axureell" alt="axureell"/>
+            <img src="axure.svg" className="axure" alt="axure"/>
         </div>
         <div className="third">
-            <img src="interell.svg" class="interell" alt="interell"/>
-            <img src="inter.svg" class="inter" alt="inter"/>
+            <img src="interell.svg" className="interell" alt="interell"/>
+            <img src="inter.svg" className="inter" alt="inter"/>
         </div>
         <div className="four">
-            <img src="capsell.svg" class="capsell" alt="capsell"/>
-            <img src="caps.svg" class="caps" alt="caps"/>
+            <img src="capsell.svg" className="capsell" alt="capsell"/>
+            <img src="caps.svg" className="caps" alt="caps"/>
         </div>
         <div className="five">
-            <img src="featherell.svg" class="featherell" alt="featherell"/>
-            <img src="feather.svg" class="feather" alt="feather"/>
+            <img src="featherell.svg" className="featherell" alt="featherell"/>
+            <img src="feather.svg" className="feather" alt="feather"/>
         </div>
         <div className="six">
-            <img src="boltell.svg" class="boltell" alt="boltell"/>
-            <img src="bolt.svg" class="bolt" alt="bolt"/>
+            <img src="boltell.svg" className="boltell" alt="boltell"/>
+            <img src="bolt.svg" className="bolt" alt="bolt"/>
         </div>
         <div className="sev">
-            <img src="galaxyell.svg" class="galaxyell" alt="galaxyell"/>
-            <img src="galaxy.svg" class="galaxy" alt="galaxy"/>
+            <img src="galaxyell.svg" className="galaxyell" alt="galaxyell"/>
+            <img src="galaxy.svg" className="galaxy" alt="galaxy"/>
         </div>
         <div className="eig">
-            <img src="fashionell.svg" class="fashionell" alt="fashionell"/>
-            <img src="fashion.svg" class="fashion" alt="fashion"/>
+            <img src="fashionell.svg" className="fashionell" alt="fashionell"/>
+            <img src="fashion.svg" className="fashion" alt="fashion"/>
         </div>
     </div>
   );
