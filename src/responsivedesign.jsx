4054c1774a8530857412a341896d86b9f22ba80a
Tithi <145503991+tithishah21@users.noreply.github.com>
2024-07-04 23:18:53 +0530

    Add files via upload

---
+++ src/responsivedesign.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+function App() {
+  return (
+    <main>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      <section className='sm:justify-center sm:items-center'>
+      <div>
+        <img src="prevsponsors.png" className='w-4/5 justify-center items-center mx-8 mt-24 mt-16'
+        />
+      </div>
+      <div className='items-center'>
+      <div className='relative md:ml-24 lg:ml-24 xl:ml-48'>
+        <img src="echoell.svg" className='absolute w-44 mt-9 object-cover rounded-full z-10 '
+        />
+        <img src="galaxy.svg" className='absolute w-28 ml-12 mt-16 object-cover rounded-full z-20'
+        />
+      </div>
+      <div className='relative md:ml-40 lg:ml-56 xl:ml-72'>
+        <img src="echoell.svg" className='absolute w-44 ml-32 mt-9 object-cover rounded-full z-10'
+        />
+        <img src="bolt.svg" className='absolute w-28 ml-48 mt-16 object-cover rounded-full z-20'
+        />
+      </div>
+      <div className='relative md:ml-64 lg:ml-96 xl:ml-96'>
+        <img src="echoell.svg" className='absolute w-44 ml-72 mt-9 object-cover rounded-full z-10'
+        />
+        <img src="axure.svg" className='absolute w-28 ml-80 mt-16 object-cover rounded-full z-20'
+        />
+      </div>
+      <div className='relative md:ml-24 lg:ml-24 xl:ml-48'>
+        <img src="echoell.svg" className='absolute w-44 mt-48 object-cover rounded-full z-10'
+        />
+        <img src="caps.svg" className='absolute w-28 ml-12 mt-56 object-cover rounded-full z-20'
+        />
+      </div>
+      <div className='relative md:ml-40 lg:ml-56 xl:ml-72'>
+        <img src="echoell.svg" className='absolute w-44 ml-40 mt-48 object-cover rounded-full z-10'
+        />
+        <img src="feather.svg" className='absolute w-28 ml-48 mt-56 object-cover rounded-full z-20'
+        />
+      </div>
+      <div className='relative md:ml-64 lg:ml-96 xl:ml-96'>
+        <img src="echoell.svg" className='absolute w-44 ml-72 mt-48 object-cover rounded-full z-10'
+        />
+        <img src="inter.svg" className='absolute w-28 ml-80 mt-56 object-cover rounded-full z-20'
+        />
+      </div>
+      <div className='relative md:ml-32 lg:ml-40 xl:ml-56'>
+        <img src="echoell.svg" className='absolute w-44 ml-20 mt-96 object-cover rounded-full z-10'
+        />
+        <img src="echo.svg" className='absolute w-28 ml-32 mt-96 object-cover rounded-full z-20'
+        />
+      </div>
+      <div className='relative md:ml-48 lg:ml-72 xl:ml-80'>
+        <img src="echoell.svg" className='absolute w-44 ml-56 mt-96 object-cover rounded-full z-10'
+        />
+        <img src="fashion.svg" className='absolute w-28 ml-64 mt-96 object-cover rounded-full z-20'
+        />
+      </div>
+      </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
