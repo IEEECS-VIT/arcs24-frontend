5475681b7af1dd58aca57213c892e58824fbfc42
ShubhamPrasad3012 <shubhamprasadclass10@gmail.com>
2024-07-15 21:14:51 +0530

    take 72

---
+++ src/Components/Combined2.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Part4 from './Part4';
+import Part5 from './Part5';
+
+export default function Combined() {
+  return (
+    <>
+      <div className='d4 text-white'>
+        <Part4 />
+        <Part5 />
+        </div>
+    </>
+  );            
+}
