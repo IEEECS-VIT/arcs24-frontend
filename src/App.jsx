45c7812915a26f7b9dddbd995d4b1991cc4f8d55
Tithi <145503991+tithishah21@users.noreply.github.com>
2024-07-22 22:26:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import LandingPage from "./Components/LandingPage";
 import Yooho from "./Components/Yooho";
 import Combined from "./Components/Combined";
 import Combined2 from "./Components/Combined2";
-import Sponsors from "./Components/responsivedesign";
+import Spons from "./Components/Spons";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import HackBattle from "./Components/HackBattle";
@@ -21,6 +21,7 @@ function App() {
       <Yooho />
       <Combined/>
       <Combined2/>
+      <Spons/>
       <Why />
       <Happie />
       <Faq />
