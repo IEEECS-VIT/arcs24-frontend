4f700e9df1a82bbc58388a7d0a41fa0ef6016718
Tithi <145503991+tithishah21@users.noreply.github.com>
2024-07-22 22:07:44 +0530

    Updated  responsivedesign.jsx

--- src/Components/responsivedesign.jsx
+++ src/Components/responsivedesign.jsx
@@ -11,51 +11,51 @@ function resp() {
       </div>
       <div className='items-center'>
       <div className='relative md:ml-24 lg:ml-24 xl:ml-48'>
-        <img src="echoell.svg" className='absolute w-44 mt-9 object-cover rounded-full z-10 '
+        <img src="echoell.svg" className='relative w-44 mt-9 object-cover rounded-full z-10 '
         />
-        <img src="galaxy.svg" className='absolute w-28 ml-12 mt-16 object-cover rounded-full z-20'
+        <img src="galaxy.svg" className='relative w-28 ml-12 mt-16 object-cover rounded-full z-20'
         />
       </div>
       <div className='relative md:ml-40 lg:ml-56 xl:ml-72'>
-        <img src="echoell.svg" className='absolute w-44 ml-32 mt-9 object-cover rounded-full z-10'
+        <img src="echoell.svg" className='relative w-44 ml-32 mt-9 object-cover rounded-full z-10'
         />
-        <img src="bolt.svg" className='absolute w-28 ml-48 mt-16 object-cover rounded-full z-20'
+        <img src="bolt.svg" className='relative w-28 ml-48 mt-16 object-cover rounded-full z-20'
         />
       </div>
       <div className='relative md:ml-64 lg:ml-96 xl:ml-96'>
-        <img src="echoell.svg" className='absolute w-44 ml-72 mt-9 object-cover rounded-full z-10'
+        <img src="echoell.svg" className='relative w-44 ml-72 mt-9 object-cover rounded-full z-10'
         />
-        <img src="axure.svg" className='absolute w-28 ml-80 mt-16 object-cover rounded-full z-20'
+        <img src="axure.svg" className='relative w-28 ml-80 mt-16 object-cover rounded-full z-20'
         />
       </div>
       <div className='relative md:ml-24 lg:ml-24 xl:ml-48'>
-        <img src="echoell.svg" className='absolute w-44 mt-48 object-cover rounded-full z-10'
+        <img src="echoell.svg" className='relative w-44 mt-48 object-cover rounded-full z-10'
         />
-        <img src="caps.svg" className='absolute w-28 ml-12 mt-56 object-cover rounded-full z-20'
+        <img src="caps.svg" className='relative w-28 ml-12 mt-56 object-cover rounded-full z-20'
         />
       </div>
       <div className='relative md:ml-40 lg:ml-56 xl:ml-72'>
-        <img src="echoell.svg" className='absolute w-44 ml-40 mt-48 object-cover rounded-full z-10'
+        <img src="echoell.svg" className='relative w-44 ml-40 mt-48 object-cover rounded-full z-10'
         />
-        <img src="feather.svg" className='absolute w-28 ml-48 mt-56 object-cover rounded-full z-20'
+        <img src="feather.svg" className='relative w-28 ml-48 mt-56 object-cover rounded-full z-20'
         />
       </div>
       <div className='relative md:ml-64 lg:ml-96 xl:ml-96'>
-        <img src="echoell.svg" className='absolute w-44 ml-72 mt-48 object-cover rounded-full z-10'
+        <img src="echoell.svg" className='relative w-44 ml-72 mt-48 object-cover rounded-full z-10'
         />
-        <img src="inter.svg" className='absolute w-28 ml-80 mt-56 object-cover rounded-full z-20'
+        <img src="inter.svg" className='relative w-28 ml-80 mt-56 object-cover rounded-full z-20'
         />
       </div>
       <div className='relative md:ml-32 lg:ml-40 xl:ml-56'>
-        <img src="echoell.svg" className='absolute w-44 ml-20 mt-96 object-cover rounded-full z-10'
+        <img src="echoell.svg" className='relative w-44 ml-20 mt-96 object-cover rounded-full z-10'
         />
-        <img src="echo.svg" className='absolute w-28 ml-32 mt-96 object-cover rounded-full z-20'
+        <img src="echo.svg" className='relative w-28 ml-32 mt-96 object-cover rounded-full z-20'
         />
       </div>
       <div className='relative md:ml-48 lg:ml-72 xl:ml-80'>
-        <img src="echoell.svg" className='absolute w-44 ml-56 mt-96 object-cover rounded-full z-10'
+        <img src="echoell.svg" className='relative w-44 ml-56 mt-96 object-cover rounded-full z-10'
         />
-        <img src="fashion.svg" className='absolute w-28 ml-64 mt-96 object-cover rounded-full z-20'
+        <img src="fashion.svg" className='relative w-28 ml-64 mt-96 object-cover rounded-full z-20'
         />
       </div>
       </div>
