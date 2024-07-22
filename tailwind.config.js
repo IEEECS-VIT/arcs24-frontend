b006c0ef6f20c8aa7efd4fa56574b1c309bc63bd
shamvrueth <145780447+shamvrueth@users.noreply.github.com>
2024-07-22 21:54:21 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,10 @@ export default {
         "blurple": "#9F9DFF",
         background: "#01061B",
       },
+        screens: {
+        'small': { 'raw': '(max-height: 800px)' },
+        'tall': { 'raw': '(min-height: 950px)' },
+      }
     },
     plugins: [],
   },
