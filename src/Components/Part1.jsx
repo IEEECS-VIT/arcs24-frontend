66df86fc8ef3d0d80e5e53a53feb216a28cadd44
ShubhamPrasad3012 <shubhamprasadclass10@gmail.com>
2024-08-01 21:23:32 +0530

    updated some thing

--- src/Components/Part1.jsx
+++ src/Components/Part1.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 export default function Part1() {
   return (
-    <div className='div1  h-full m-0 lg:flex lg:mt-0 lg:ml-[2.2552083333333335vw]'>
-      
-      <img className='img1 mt-0 ml-[2vw] lg:ml-[0vw] h-[0] w-[0] lg:h-[51.45891276864728vh] lg:w-[46vw] ' src="Group115.svg" alt='' />
+    <div className='div1 relative h-full m-0 lg:flex lg:mt-0 lg:ml-[2.2552083333333335vw]'>
+      <img className='img1 absolute z-5 mt-0 ml-[2vw] lg:ml-[4vw] h-[0] w-[0] lg:h-[51.45891276864728vh] lg:w-[30vw]' src="Group222.png" alt='' />
+      <img className='img25 relative h-0 z-10 ml-[5vw] lg:h-[25vw]' src='Group223.png'></img>
+      <img className='imh26 relative h-0 z=15 mr-[5vw] lg:h-[35vh] mt-[6vh]' src='Group141.png'></img>   
       <img className='img10 mt-[0vh] ml-[22.5vw] h-[25vh] lg:hidden' src='Group117.svg' alt='' />
       <div className='d2 mt-[2.5vh] lg:ml-[2.5vw] lg:mt-[2vw] flex '>
         <img className='img2 ml-[1vw] h-[6.7926675094816686vh] w-[7.2135416666666665vw] lg:h-[8.099873577749683vh] lg:w-[3.958984375vw] lg:m-[0.3255208333333333vw] lg:mr-[0]' src="Group111.svg" alt='' />
@@ -15,10 +16,10 @@ export default function Part1() {
           <p className='p2 text-sm lg:text-3xl font-source-serif font-semibold leading-[151%] tracking-[0.2604166666666667vw] mt-[0.5vw] m-0'>turpis sit enim dictum interdum quis.</p>
           <p className='p2 lg:ml-[0] text-sm lg:text-3xl font-source-serif font-semibold leading-[151%] mt-[0.5vw] tracking-[0.2604166666666667vw] m-0'>Tortor sed et sed pretium ultrices. Et</p>
           <p className='p2 lg:ml-[0] text-sm lg:text-3xl font-source-serif font-semibold leading-[151%] mt-[0.5vw] tracking-[0.2604166666666667vw] m-0'>sit amet egestas <span className='t1 text-blurple'>aliquam nunc</span>.</p>
-        </div>
-        <img className='img3 mt-[40vw] lg:mr-[5.90625vw] lg:mt-[37.92667509481669vh] h-[6.7926675094816686vh] w-[7.2135416666666665vw] lg:h-[8.099873577749683vh] lg:w-[3.958984375vw]' src="/Group112.svg" alt='' />
+        </div>  
+        <img className='img3 mt-[40vw] lg:mr-[0.90625vw] lg:mt-[37.92667509481669vh] h-[6.7926675094816686vh] w-[7.2135416666666665vw] lg:h-[8.099873577749683vh] lg:w-[3.958984375vw]' src="/Group112.svg" alt='' />
       </div>
-      
+        
     </div>
   );
 }
