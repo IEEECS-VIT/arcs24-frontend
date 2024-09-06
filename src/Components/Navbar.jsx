6a5d69206de6607328c1e89c733cd6550b3da2c7
Medhansh Jain <medhanshjain2010@gmail.com>
2024-09-06 22:31:12 +0530

    updated links

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
 
   return (
     <>
-      <div className="fixed top-0 z-50 hidden items-center justify-center px-48 pt-4 md:flex w-full">
+      <div className="fixed top-0 z-50 hidden w-full items-center justify-center px-48 pt-4 lg:flex">
         <img src="navbar.svg" className="w-full" alt="Navbar"></img>
         <nav className="absolute top-0 ml-[30vw] w-full px-48 pt-4">
           <ul className="mb-8 flex flex-row justify-center gap-x-10">
@@ -28,15 +28,10 @@ export default function Navbar() {
                 FAQ
               </a>
             </li>
-            <li>
-              <a href="#" className="font-rugen text-base text-white">
-                Contact Us
-              </a>
-            </li>
           </ul>
         </nav>
       </div>
-      <div className="fixed top-0 z-50 flex w-full items-center justify-between px-4 pt-4 md:hidden">
+      <div className="fixed top-0 z-50 flex w-full items-center justify-between px-4 pt-4 lg:hidden">
         <img src="logo.svg" alt="Logo" className="h-8" />{" "}
         <img src="menu.svg" alt="Menu" className="h-8" onClick={toggleMenu} />{" "}
       </div>
--- src/Components/Part2.jsx
+++ src/Components/Part2.jsx
@@ -5,40 +5,45 @@ export default function Part2() {
     <div className="mt-[2.5vh] lg:flex">
       <div className="content1 ml-[17vw] mt-[4vh] w-[61.3828125vw] lg:ml-[8.5vw] lg:mt-[5.7926675094816686vh] lg:w-[27.799479166666668vw]">
         <p className="p1 ml-[5vw] w-[57.3828125vw] font-source-serif text-[1.463rem] font-bold leading-[145%] tracking-[0.3604166666666667vw] lg:ml-[0] lg:mt-[0.6321112515802781vh] lg:w-[27.799479166666668vw] lg:text-xl">
-          <span className="t1 text-blurple">Date: </span>To be announced
+          <span className="t1 text-blurple">Date: </span>25-26th September
         </p>
         <p className="p1 ml-[7vw] w-[57.3828125vw] font-source-serif text-2xl font-bold leading-[145%] tracking-[0.3604166666666667vw] lg:ml-[0] lg:mt-[0.6321112515802781vh] lg:w-[27.799479166666668vw] lg:text-xl">
-          <span className="t1 text-blurple">Venue: </span>To be announced..
+          <span className="t1 text-blurple">Venue: </span>Anna Auditorium
         </p>
         <p className="p1 ml-[5vw] w-[77.3828125vw] font-source-serif text-2xl font-bold leading-[145%] tracking-[0.3604166666666667vw] lg:ml-[0] lg:mt-[0.6321112515802781vh] lg:w-[27.799479166666668vw] lg:text-xl">
-          <span className="t1 text-blurple">Price: </span>To be announced..
+          <span className="t1 text-blurple">Price: </span>₹250
         </p>
       </div>
 
       <button className="flex items-center justify-center font-normal uppercase no-underline transition-all duration-200 ease-in-out">
-        <svg
-          className="img5 mb-[10vh] ml-[7.5vw] mt-[2vw] h-[16.321112515802781vh] w-[85.182291666666668vw] lg:ml-[10.067708333333334vw] lg:mt-[9.578002528445005vh] lg:h-[6.826801517067004vh] lg:w-[35.807291666666664vw]"
-          preserveAspectRatio="xMidYMid meet"
+        <a
+          href="https://gravitas.vit.ac.in/events/e748d506-415c-4166-b45f-7485c25406aa"
+          target="_blank"
         >
-          <image
-            href="./reg.svg"
-            width="100%"
-            height="100%"
-            className="glow-on-hover"
-          />
-          <text
-            x="50%"
-            y="50%"
-            dx="4"
-            dy="-3"
-            dominantBaseline="middle"
-            textAnchor="middle"
-            fill="white"
-            className="font-rugen text-[3vh]"
+          <svg
+            className="img5 mb-[10vh] ml-[7.5vw] mt-[2vw] h-[16.321112515802781vh] w-[85.182291666666668vw] lg:ml-[10.067708333333334vw] lg:mt-[9.578002528445005vh] lg:h-[6.826801517067004vh] lg:w-[35.807291666666664vw]"
+            preserveAspectRatio="xMidYMid meet"
           >
-            Register Now
-          </text>
-        </svg>
+            <image
+              href="./reg.svg"
+              width="100%"
+              height="100%"
+              className="glow-on-hover"
+            />
+            <text
+              x="50%"
+              y="50%"
+              dx="4"
+              dy="-3"
+              dominantBaseline="middle"
+              textAnchor="middle"
+              fill="white"
+              className="font-rugen text-[3vh]"
+            >
+              Register Now
+            </text>
+          </svg>
+        </a>
       </button>
     </div>
   );
