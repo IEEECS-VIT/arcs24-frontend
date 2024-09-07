175d561adb2a3fb9e748ec4b5e328aac79e3a0cc
ShubhamPrasad3012 <shubhamprasadclass10@gmail.com>
2024-09-07 18:18:24 +0530

    removed overlapping

--- src/Components/Part1.jsx
+++ src/Components/Part1.jsx
@@ -2,51 +2,52 @@ import React from "react";
 
 export default function Part1() {
   return (
-    <div className="div1 relative m-0 h-full lg:ml-[2.2552083333333335vw] lg:mt-0 lg:flex">
+    <div className="div1 relative m-0 h-full md:ml-[2.2552083333333335vw] md:mt-0 md:flex">
       <img
-        className="img1 z-5 absolute ml-[2vw] mt-0 h-[0] w-[0] lg:ml-[4vw] lg:h-[51.45891276864728vh] lg:w-[27vw]"
+        className="img1 z-5 absolute ml-[2vw] mt-0 h-[0] w-[0] md:ml-[4vw] lg:h-[51.45891276864728vh] lg:w-[27vw] lg:mt-[0] md:h-[41vh] md:w-[27vw] md:mt-[12vh]"
         src="Group222.png"
         alt=""
       />
       <img
-        className="img25 relative z-10 ml-[2.5vw] h-0 lg:h-[25vw]"
+        className="img25 relative z-10 ml-[2.5vw] h-0 lg:h-[25vw] md:h-[30vw] md:w-[25vw] md:mt-[12vh] lg:mt-[0]"
         src="Group223.png"
       ></img>
-      <img
-        className="imh26 z=15 relative mr-[5vw] mt-[6vh] h-0 lg:h-[35vh]"
+      <img  
+        className="imh26 z=15 relative mr-[5vw] mt-[8vh]  lg:mt-[6vh] sm:mt-[0vh] h-0 lg:h-[35vh] md:h-[30vh] md:mt-[15vh]"
         src="Group141.png"
       ></img>
       <img
-        className="img10 ml-[22.5vw] mt-[0vh] h-[25vh] lg:hidden"
+        className="img10 ml-[22.5vw] mt-[0vh] h-[25vh] md:hidden"
         src="Group117.svg"
         alt=""
       />
-      <div className="d2 mt-[2.5vh] flex lg:ml-[5vw] lg:mt-[2vw]">
+      <div className="d2 mt-[2.5vh] flex md:ml-[5vw] sm:ml-[10vw] md:mt-[2vw]">
         <img
-          className="img2 ml-[1vw] h-[6.7926675094816686vh] w-[7.2135416666666665vw] lg:m-[0.3255208333333333vw] lg:mr-[0] lg:h-[8.099873577749683vh] lg:w-[3.958984375vw]"
-          src="Group111.svg"
+          className="img2 ml-[1vw] h-[6.7926675094816686vh] w-[7.2135416666666665vw] md:m-[0.3255208333333333vw] md:mr-[0] md:h-[8.099873577749683vh] md:w-[3.958984375vw]"
+          src="Group111.svg"  
           alt=""
+
         />
-        <div className="d6 mt-[8vw] w-[81.744791666666668vw] text-center lg:mt-[5.849557522123893vh] lg:w-[51.744791666666668vw] lg:text-start">
-          <p className="p2 m-0 font-source-serif text-sm font-semibold tracking-[0.2604166666666667vw] lg:ml-[0] lg:text-3xl">
+        <div className="d6 mt-[8vw]  w-[81.744791666666668vw] sm:w-[50vw] text-center md:mt-[5.849557522123893vh] md:w-[51.744791666666668vw] md:text-start lg:text-[1.875rem] md:text-[1.5rem] sm:text-[0.8rem]">
+          <p className="p2 m-0 font-source-serif text-md font-semibold tracking-[0.2604166666666667vw] md:ml-[0] md:text-3xl">
             Join the adrenaline-pumping 36-hour
           </p>
-          <p className="p2 m-0 font-source-serif text-sm font-semibold tracking-[0.2604166666666667vw] lg:ml-[0] lg:text-3xl">
+          <p className="p2 m-0 font-source-serif text-md font-semibold tracking-[0.2604166666666667vw] md:ml-[0] md:text-3xl">
             HackBattle by IEEECS-VIT, where
           </p>
-          <p className="p2 m-0 mt-[0.5vw] font-source-serif text-sm font-semibold leading-[151%] tracking-[0.2604166666666667vw] lg:ml-[0] lg:text-3xl">
+          <p className="p2 m-0 mt-[0.5vw] font-source-serif text-md font-semibold leading-[151%] tracking-[0.2604166666666667vw] md:ml-[0] md:text-3xl">
             tech innovators gather for
           </p>
-          <p className="p2 m-0 mt-[0.5vw] font-source-serif text-sm font-semibold leading-[151%] tracking-[0.2604166666666667vw] lg:ml-[0] lg:text-3xl">
+          <p className="p2 m-0 mt-[0.5vw] font-source-serif text-md font-semibold leading-[151%] tracking-[0.2604166666666667vw] md:ml-[0] md:text-3xl">
             groundbreaking challenges, keynote
           </p>
-          <p className="p2 m-0 mt-[0.5vw] font-source-serif text-sm font-semibold leading-[151%] tracking-[0.2604166666666667vw] lg:text-3xl">
+          <p className="p2 m-0 mt-[0.5vw] font-source-serif text-md font-semibold leading-[151%] tracking-[0.2604166666666667vw] md:text-3xl">
             sessions, and engaging activities that ignite creativity and drive
             innovation .
           </p>
         </div>
         <img
-          className="img3 mt-[40vw] h-[6.7926675094816686vh] w-[7.2135416666666665vw] lg:mr-[5.25625vw] lg:mt-[37.92667509481669vh] lg:h-[8.099873577749683vh] lg:w-[3.958984375vw]"
+          className="img3 mt-[40vw] sm:mt-[22.5vh] h-[6.7926675094816686vh] w-[7.2135416666666665vw] md:mr-[5.25625vw] md:mt-[37.92667509481669vh] md:h-[8.099873577749683vh] md:w-[3.958984375vw]"
           src="/Group112.svg"
           alt=""
         />
--- src/Components/Part5.jsx
+++ src/Components/Part5.jsx
@@ -2,27 +2,27 @@ import React from "react";
 
 export default function Part5() {
   return (
-    <div className="mt-[0vh] lg:flex">
-      <div className="content1 order-1 ml-[17vw] mt-[0vh] w-[61.3828125vw] lg:order-2 lg:ml-[19.5vw] lg:mt-[3.7926675094816686vh] lg:w-[27.799479166666668vw]">
-        <p className="p1  w-[87.3828125vw] font-source-serif text-[1.463rem] font-bold leading-[145%] tracking-[0.3604166666666667vw] lg:ml-[0] lg:mt-[0.6321112515802781vh] lg:w-[27.799479166666668vw] lg:text-xl">
+    <div className="mt-[0vh] md:flex">
+      <div className="content1 order-1 ml-[17vw] mt-[0vh] w-[61.3828125vw] md:order-2 md:ml-[19.5vw] md:mt-[3.7926675094816686vh] md:w-[27.799479166666668vw]">
+        <p className="p1   w-[87.3828125vw] font-source-serif text-[1.463rem] font-bold leading-[145%] tracking-[0.3604166666666667vw] md:ml-[0] md:mt-[0.6321112515802781vh] md:w-[27.799479166666668vw] md:text-xl">
           <span className="t1 ml-[2.8vw] text-blurple">Date: </span>22nd
-          September
+          September 
         </p>
-        <p className="p1 ml-[1.5vw] w-[87.3828125vw] font-source-serif text-2xl font-bold leading-[145%] tracking-[0.3604166666666667vw] lg:ml-[0] lg:mt-[0.6321112515802781vh] lg:w-[27.799479166666668vw] lg:text-xl">
+        <p className="p1 ml-[1.5vw] w-[87.3828125vw] font-source-serif text-2xl font-bold leading-[145%] tracking-[0.3604166666666667vw] md:ml-[0] md:mt-[0.6321112515802781vh] md:w-[27.799479166666668vw] md:text-xl">
           <span className="t1 ml-[2.8vw] text-blurple">Venue: </span>TT Voc
         </p>
-        <p className="p1  w-[77.3828125vw] font-source-serif text-2xl font-bold leading-[145%] tracking-[0.3604166666666667vw] lg:ml-[0] lg:mt-[0.6321112515802781vh] lg:w-[27.799479166666668vw] lg:text-xl">
+        <p className="p1  w-[77.3828125vw] font-source-serif text-2xl font-bold leading-[145%] tracking-[0.3604166666666667vw] md:ml-[0] md:mt-[0.6321112515802781vh] md:w-[27.799479166666668vw] md:text-xl">
           <span className="t1 ml-[2.8vw] text-blurple">Price: </span>₹200
         </p>
       </div>
 
-      <button className="flex items-center justify-center font-normal uppercase no-underline">
+      <button className="flex items-center justify-center font-normal uppercase no-underline sm:mb-[5vh] !important">
         <a
           href="https://gravitas.vit.ac.in/events/5387f16e-b092-470a-b918-e89307526ebf"
           target="_blank"
         >
           <svg
-            className="order-2 mb-[4vh] ml-[7.5vw] mt-[2vw] h-[16.321112515802781vh] w-[85.182291666666668vw] lg:order-1 lg:mb-[8vh] lg:ml-[10.067708333333334vw] lg:mt-[9.578002528445005vh] lg:h-[6.826801517067004vh] lg:w-[35.807291666666664vw]"
+            className="order-2 ml-[7.5vw] mt-[2vw] h-[16.321112515802781vh] w-[85.182291666666668vw] md:order-1 md:ml-[10.067708333333334vw] md:mt-[9.578002528445005vh] lg:h-[6.826801517067004vh] lg:w-[35.807291666666664vw] md:w-[40vw] md:h-[5vh]"
             preserveAspectRatio="xMidYMid meet"
           >
             <image
